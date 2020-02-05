e7f824e64546b221a8bcb29311a48af2101b4ea6
kingsman142 <kingsman142@yahoo.com>
2020-02-04 21:28:59 -0500

    added comments to train.py and added code to save the model after training

--- README.md
+++ README.md
@@ -5,12 +5,16 @@ Using an LSTM to predict stock prices
 1. Run train.py --stocks [stock_name]
 
 TODO:
-Implement exponential moving average (EMA)
-Investigate common fault signals in stock prediction ( https://www.investopedia.com/terms/f/false-signal.asp and https://www.investopedia.com/articles/active-trading/052014/how-use-moving-average-buy-stocks.asp)
-Make a small trading bot and show applicability
+* Implement exponential moving average (EMA)
+* Investigate common fault signals in stock prediction ( https://www.investopedia.com/terms/f/false-signal.asp and https://www.investopedia.com/articles/active-trading/052014/how-use-moving-average-buy-stocks.asp)
+* Make a small trading bot and show applicability
+* Maybe try SGD optimizer over Adam optimizer
 
 Possible analysis:
-Plot effect of simple moving average
-Plot effect of exponential moving average
-Show results on predicting of one stock
-Show results on predicting of multiple stocks
+* Plot effect of simple moving average
+* Plot effect of exponential moving average
+* Show results on predicting of one stock
+* Show results on predicting of multiple stocks
+* Investigate use of L1 vs. MSE loss
+* Write study based around performance of different window sizes (or even hidden state sizes, batch sizes, etc.)
+* Investigate whether there's a difference in fluctuations/spikes between popular stocks and non-popular stocks
--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import numpy as np
 from model import StockPredictor
 from dataset import StockDataset
 
+# set network hyperparameters
 TRAIN = 0.8 #0.0209
 TEST = 0.2
 WINDOW_SIZE = 50 #250 #250
@@ -17,34 +18,50 @@ BATCH_SIZE = 1 #4
 HIDDEN_SIZE = 150 #200
 LEARNING_RATE = 0.001 #0.0001
 
-# train = 0.8, window = 50, epochs = 20, batch size = 1, hidden size = 100, lr = 0.001, no moving average => 0.005 L1 loss ('sum' reduction)
-# train = 0.8, window = 50, epochs = 20, batch size = 1, hidden size = 100, lr = 0.001, simple moving average => 0.0022 L1 loss ('sum' reduction)
+MODEL_LOAD_NAME = None # change to load in a custom model
+MODEL_SAVE_NAME = "train{}_windowsize{}_epochs{}_batchsize{}_hiddensize{}_lr{}".format(int(TRAIN*100), WINDOW_SIZE, EPOCHS, BATCH_SIZE, HIDDEN_SIZE, LEARNING_RATE)
 
+# train = 0.8, window = 50, epochs = 20, batch size = 1, hidden size = 100, lr = 0.001, no moving average, 1 stock => 0.005 L1 loss ('sum' reduction)
+# train = 0.8, window = 50, epochs = 20, batch size = 1, hidden size = 100, lr = 0.001, simple moving average, 1 stock => 0.0022 L1 loss ('sum' reduction)
+# train = 0.8, window = 50, epochs = 20, batch size = 1, hidden size = 100, lr = 0.001, simple moving average, 2 stocks => 0.0021 L1 loss ('sum' reduction)
+# train = 0.8, window = 50, epochs = 20, batch size = 1, hidden size = 100, lr = 0.001, simple moving average, 6 stocks => 0.0013 L1 loss ('sum' reduction)
+
+# set up dataset and model
+stock_fns = ["aa.us.txt", "msft.us.txt", "goog.us.txt", "gpic.us.txt", "rfdi.us.txt", "aal.us.txt"] # chosen somewhat randomly
 model = StockPredictor(hidden_size = HIDDEN_SIZE)
-dataset = StockDataset(stock_fns = "aa.us.txt", window_size = WINDOW_SIZE, train = TRAIN)
-#dataset.plot_stock_raw() # (OPTIONAL) uncomment to plot the stock data -- NOTE: only plots the first stock's history if a list of stocks is provided
+dataset = StockDataset(stock_fns = stock_fns, window_size = WINDOW_SIZE, train = TRAIN)
 
+# (OPTIONAL) uncomment to plot the stock data -- NOTE: only plots the first stock's history if a list of stocks is provided
+#dataset.plot_stock_raw()
+
+# load pre-trained model weights
+if MODEL_LOAD_NAME is not None:
+    model.load_state_dict(torch.load(os.path.join("models", MODEL_LOAD_NAME)))
+
+# set up hyperparameters
 optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE)
 loss_func = nn.L1Loss(reduction = 'mean') #nn.MSELoss(reduction = 'sum')
-
 loader = data.DataLoader(dataset, batch_size = BATCH_SIZE, shuffle = True)
 
-for epoch in range(EPOCHS):
+# train the model
+for epoch in range(EPOCHS): # iterate over epochs
     avg_loss = 0.0
-    for batch_id, samples in enumerate(loader):
+    for batch_id, samples in enumerate(loader): # iterate over batches
+        # input prices and ground-truth price prediction
         prices = samples['prices']
         labels = samples['labels']
 
+        # make predictions and calculate loss
         pred = model(prices)
-
         loss = loss_func(pred, labels)
 
+        # backpropagate loss
         model.zero_grad()
         loss.backward()
         optimizer.step()
 
+        # print out useful logging information for user
         avg_loss += loss.item()
-
         if batch_id % 50 == 0:
             if BATCH_SIZE == 1:
                 print("Epoch {}/{} -- Batch {}/{} -- Loss: {} -- Pred: {}, True: {}".format(epoch+1, EPOCHS, batch_id+1, len(loader), loss.item(), pred.item(), labels.item()))
@@ -53,4 +70,7 @@ for epoch in range(EPOCHS):
     avg_loss /= len(loader)
     print("--- Epoch {} avg loss: {}".format(epoch+1, avg_loss))
 
-# NOTE: pytorch LSTM units take input in the form of [window_length, batch_size, num_features], which will end up being [WINDOW_SIZE, batch_size, 1] for our dataset
+# save our model
+if not os.path.exists("models"):
+    os.mkdir("models")
+torch.save(model.state_dict(), "models/{}".format(MODEL_SAVE_NAME))
