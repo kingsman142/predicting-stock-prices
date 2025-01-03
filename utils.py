9861938186d73ed7f7fe9cedf4437cd152afd4f5
kingsman142 <jamhahn@yahoo.com>
2025-01-02 21:34:53 -0800

    Update plot_stock_clean to accept new data format

--- utils.py
+++ utils.py
@@ -10,8 +10,8 @@ def plot_stock_raw(stock_data):
 
 # only used for data with only number (such as vanilla python lists)
 def plot_stock_clean(stock_data):
-    plt.plot(range(len(stock_data)), stock_data)
-    plt.xticks(range(0, len(stock_data), 500), range(0, len(stock_data), 500), rotation = 45)
+    plt.plot(range(0, len(stock_data[0])), stock_data[0])
+    plt.xticks(range(0, len(stock_data[0]), 1), range(0, len(stock_data[0]), 1), rotation = 45)
     plt.xlabel('Timestep', fontsize = 10)
     plt.ylabel('Avg. Price', fontsize = 11)
     plt.show()
